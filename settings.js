8e483d7a82c59dbb260e745954eefaea376e66f2
SunDall <98078946+SunDall@users.noreply.github.com>
2026-04-25 01:16:50 +0300

    Added new functional buttons and improved UX
    Also removed non-MASMZE-3D characters

--- settings.js
+++ settings.js
@@ -3,11 +3,10 @@ function setSetting(key, value) {
   settings[key] = value;
   localStorage.setItem("settings", JSON.stringify(settings));
   window.dispatchEvent(new CustomEvent("settingsChanged", {
-    detail: {key, newValue: value}
+    detail: { key, newValue: value }
   }));
 }
-
-function createMenu(){
+function createMenu() {
   const settingsTopContainer = document.createElement("div");
   settingsTopContainer.className = "settingsTopContainer";
   const menu = document.getElementById("menu");
@@ -27,37 +26,56 @@ function createMenu(){
   sizeSlider.min = "10";
   sizeSlider.max = "120";
   sizeSlider.value = "20";
-  
   const sizeLabel = document.createElement("label");
   sizeLabel.className = "sizeLabel";
   sizeLabel.textContent = "Picture size: " + sizeSlider.value;
   sizeBlock.appendChild(sizeLabel);
   sizeBlock.appendChild(sizeSlider);
   settingsTopContainer.appendChild(sizeBlock);
-  
+
   const clearPlayground = document.createElement("button");
   clearPlayground.className = "clearPlayground";
   clearPlayground.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   menu.appendChild(clearPlayground);
 
+  const refreshPlayground = document.createElement("button");
+  refreshPlayground.className = "refreshPlayground";
+  refreshPlayground.innerHTML = '<i class="fa-solid fa-rotate-left"></i>';
+  menu.appendChild(refreshPlayground);
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.className = 'input';
+  input.value = 'Paste JSON-styled text to add images and press the button below [{"name": "GreatCorn", "url": "https://avatars.githubusercontent.com/u/31311274?v=4"}]'
+  menu.appendChild(input);
+
   const addPicsButton = document.createElement("button");
   addPicsButton.className = "addPicsButton";
   addPicsButton.innerHTML = '<i class="fa-solid fa-plus"></i>';
   menu.appendChild(addPicsButton);
-  
+
   const fileInput = document.getElementById("fileInput");
-  addPicsButton.addEventListener("click", () => {fileInput.click();});
+  addPicsButton.addEventListener("click", () => {
+    if (input.value != 'Paste JSON-styled text to add images and press the button below [{"name": "GreatCorn", "url": "https://avatars.githubusercontent.com/u/31311274?v=4"}]' || input.value != "" || input.value != " ") {
+      try {
+        playgroundCards(input.value);
+      } catch (err) {
+        alert("Cannot create valid JSON due to: " + err.message);
+      }
+    } else { fileInput.click(); }
+  });
   fileInput.addEventListener("change", () => {
     const files = Array.from(fileInput.files);
     const paths = files.map(file => ({
-      name: file.name, 
-      url: URL.createObjectURL(file) 
+      name: file.name,
+      url: URL.createObjectURL(file)
     }));
     const jsonText = JSON.stringify(paths, null, 2);
     playgroundCards(jsonText);
   });
-  
+
 }
+
 
 createMenu();
 
@@ -68,22 +86,25 @@ const settingsButton = document.querySelector(".settingsButton");
 const sizeBlock = document.querySelector(".sizeBlock");
 const addPicsButton = document.querySelector("addPicsButton");
 
-[settingsButton, gear].forEach(element =>{
+[settingsButton, gear].forEach(element => {
   element.addEventListener("click", (event) => {
     menu.classList.toggle("active");
     event.stopPropagation();
     sizeBlock.classList.toggle("active");
     gear.classList.toggle("activated");
 
-    let clearPlayground = document.querySelector(".clearPlayground")
-    if(clearPlayground.innerHTML === "Clear Playground"){
+    let clearPlayground = document.querySelector(".clearPlayground");
+    if (clearPlayground.innerHTML === "Clear Playground") {
       clearPlayground.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    } else setTimeout(() => {clearPlayground.innerHTML = 'Clear Playground';}, 100);
+    } else setTimeout(() => { clearPlayground.innerHTML = 'Clear Playground'; }, 100);
 
-    let addPicsButton = document.querySelector(".addPicsButton")
-    if(addPicsButton.innerHTML === 'Add Images'){
+    let addPicsButton = document.querySelector(".addPicsButton");
+    if (addPicsButton.innerHTML === 'Add Images') {
       addPicsButton.innerHTML = '<i class="fa-solid fa-plus"></i>';
-    } else setTimeout(() => {addPicsButton.innerHTML = 'Add Images';}, 100);
+    } else setTimeout(() => { addPicsButton.innerHTML = 'Add Images'; }, 100);
+
+    let input = document.querySelector('.input');
+    input.classList.toggle("active");
   })
 });
 
@@ -94,5 +115,18 @@ sizeSlider.addEventListener("input", () => {
 });
 
 document.querySelector(".clearPlayground").addEventListener("click", () => {
-    document.getElementById("playGround").innerHTML = "";
+  document.getElementById("playGround").innerHTML = "";
 });
+
+document.querySelector(".refreshPlayground").addEventListener("click", () => {
+  const cards = document.querySelectorAll(".card.clicked");
+  //console.log(cards);
+  cards.forEach(card => {
+    card.classList.remove('clicked');
+    const existingSpan = card.querySelector(".overlay-text");
+    if (existingSpan) {
+      existingSpan.remove();
+    }
+  });
+})
+
